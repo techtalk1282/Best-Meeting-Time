259b8afc401e70e6620d670069a2b953a26e3145
JC Portfolio <155587612+techtalk1282@users.noreply.github.com>
2026-03-13 23:58:51 -0400

    connect ToolPreviewSection to LayoutShell toolPreview slot

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 
 import LayoutShell from "./ui/LayoutShell";
 import HeroSection from "./ui/HeroSection";
+import ToolPreviewSection from "./ui/ToolPreviewSection";
 import PremiumFeaturesSection from "./ui/PremiumFeaturesSection";
 import BonusFeaturesSection from "./ui/BonusFeaturesSection";
 import FooterSection from "./ui/FooterSection";
@@ -11,7 +12,7 @@ export default function HomePage() {
   return (
     <LayoutShell
       hero={<HeroSection />}
-      toolPreview={null}
+      toolPreview={<ToolPreviewSection />}
       premiumFeatures={<PremiumFeaturesSection />}
       bonusFeatures={<BonusFeaturesSection />}
       footer={<FooterSection />}
