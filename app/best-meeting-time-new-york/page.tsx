61b5d4225af0f43dca92a5355082fe149321f88c
JC Portfolio <155587612+techtalk1282@users.noreply.github.com>
2026-04-03 17:38:12 -0400

    tune: increase premium section offset to fully clear initial viewport on city page 250

--- app/best-meeting-time-new-york/page.tsx
+++ app/best-meeting-time-new-york/page.tsx
@@ -99,7 +99,7 @@ export default function NewYorkMeetingPage() {
             </div>
 
             {/* REQUIRED FOR GATING SCROLL */}
-            <div id="premium-features" style={{ marginTop: "200px" }}>
+            <div id="premium-features" style={{ marginTop: "250px" }}>
               <PremiumFeaturesSection />
             </div>
 
