3d3e8b3af682c549d6c0baab9ce27a0a5b1d9ad7
JC Portfolio <155587612+techtalk1282@users.noreply.github.com>
2026-05-10 21:56:08 -0400

    Update Features nav label to Schedule a Meeting

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -34,7 +34,7 @@ export default function FeaturesPage() {
 
         <nav style={nav} aria-label="Features page navigation">
           <a href="/" style={navLink}>Home</a>
-         <a href="/how-it-works" style={navLink}>How It Works</a>
+         <a href="/how-it-works" style={navLink}>Schedule a Meeting</a>
           <a href="/features" style={navLink}>Features</a>
           <a href="/guides" style={navLink}>Guides</a>
           <a href="/blog" style={navLink}>Blog</a>
